5e216e128b5d51497f06504fb3ddd24d29de29b3
pfeuh <ze.pfeuh@gmail.com>
2020-07-30 09:14:37 +0200

    some functions name more explicit

--- tools/doTheJob.py
+++ tools/doTheJob.py
@@ -13,7 +13,7 @@ import subprocess
 ATARI_DISK_NAME = "./../out/DISK.XFD"
 VERBOSE = "-verbose" in sys.argv
 
-def copyFileToToAtari(ifname, ofname, disk):
+def copyPcFileToAtariDisk(ifname, ofname, disk):
     global FILE_SHOWED
     if VERBOSE:
         sys.stdout.write("copying %s -> %s->%s\n"%(ifname, disk.name, ofname))
@@ -21,7 +21,7 @@ def copyFileToToAtari(ifname, ofname, disk):
         bytes = fp.read(-1)
     disk.writeFile(ofname, bytes)
 
-def copyAtariToToAtari(ifname, idiskname, ofname, odisk):
+def copyAtariFileToAtariDisk(ifname, idiskname, ofname, odisk):
     if VERBOSE:
         sys.stdout.write("copying %s->%s -> %s->%s\n"%(idiskname, ifname, odisk.name, ofname))
     temp_disk = VIRTUAL_DISK(idiskname)
@@ -48,10 +48,10 @@ if errnum:
     sys.exit(errnum)
 disk = VIRTUAL_DISK(ATARI_DISK_NAME)
 #add some fonts and credits on disk
-copyAtariToToAtari("ACCENTUE.FNT", "./TITREUSE", "ACCENTUE.FNT", disk)
-copyAtariToToAtari("KAISER.FNT", "./TITREUSE", "KAISER.FNT", disk)
-copyAtariToToAtari("NIKON.SCR", "./TITREUSE", "NIKON.SCR", disk)
-copyAtariToToAtari("AIDE.ECR", "./TITREUSE", "HELP.SCR", disk)
+copyAtariFileToAtariDisk("ACCENTUE.FNT", "./TITREUSE", "ACCENTUE.FNT", disk)
+copyAtariFileToAtariDisk("KAISER.FNT", "./TITREUSE", "KAISER.FNT", disk)
+copyAtariFileToAtariDisk("NIKON.SCR", "./TITREUSE", "NIKON.SCR", disk)
+copyAtariFileToAtariDisk("AIDE.ECR", "./TITREUSE", "HELP.SCR", disk)
 
 #compile .C to binary file
 params = ["./compile.sh"]
@@ -60,10 +60,10 @@ if errnum:
     sys.exit(errnum)
 
 #write the executable on Atari disk
-copyFileToToAtari("./../src/CREDITS.COM", "CREDITS.COM", disk)
-copyFileToToAtari("./../src/CREDITS.COM", "A", disk)
+copyPcFileToAtariDisk("./../src/CREDITS.COM", "CREDITS.COM", disk)
+copyPcFileToAtariDisk("./../src/CREDITS.COM", "A", disk)
 sys.stdout.write(getInfo(ATARI_DISK_NAME,  "A"))
-#~ copyFileToToAtari("./../src/CREDITS.COM", "DUP.SYS", disk)
+#~ copyPcFileToAtariDisk("./../src/CREDITS.COM", "DUP.SYS", disk)
 
 params = ["atari800"]
 params.append("-nobasic")
